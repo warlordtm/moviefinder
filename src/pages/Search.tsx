77765648e8afc2f70d7e3ea6d65c39d4570ea7ba
Godwin Bamisaye <godwinbamisaye3@gmail.com>
2025-07-16 20:56:34 +0100

    Fix bug

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -4,8 +4,6 @@ import MovieCard from '../components/MovieCard';
 import { search_movies, get_popular_movies } from '../services/tmdb'
 import { useDebounce } from '../hooks/useDebounce';
 import '../styles/Pagination.css'
-import Footer from '../components/Footer';
-
 
 
 function Search()
